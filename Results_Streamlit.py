264eb872daf665f01fb59d5f7182a8fe51002d96
shruthic318 <x23248556@student.ncirl.ie>
2024-12-22 12:49:17 +0000

    Update Results_Streamlit.py

--- Results_Streamlit.py
+++ Results_Streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
 
 #Load the data to a dataframe
@@ -12,4 +12,25 @@ st.title("House Price Prediction Dataset-from Quicker")
 
 st.dataframe(df.head(5))
 
+# Filter Options
+numberofbhk = st.sidebar.selectbox("Select NumberOfBHK:", df['NumberOfBHK'].unique())
+transaction = st.sidebar.selectbox("Select Transaction:", df['Transaction'].unique())
+availability = st.sidebar.selectbox("Select Availability:", df['Availability'].unique())
+date = st.date_input("Select Posted Date:")
+df['PostedDate'] = pd.to_datetime(df['PostedDate'], format='%Y-%m-%d %H:%M:%S')
+unique_dates = df['last_updated'].dt.date.unique()
+date_filter = st.sidebar.selectbox('Date', unique_dates)
+postedby = st.sidebar.selectbox("Select Posted By:", df['PostedBy'].unique())
+reraapproved = st.sidebar.selectbox("Select Rera Appproved:", df['ReraApproved'].unique())
+areaname = st.sidebar.selectbox("Select Area Name:", df['AreaName'].unique())
+filtered_df = df[(df['PostedDate'].dt.date == pd.to_datetime(date_filter).date()) 
+                & (df['NumberOfBHK'] == numberofbhk)
+                & (df['Transaction'] == transaction)
+                & (df['Availability'] == availability)
+                & (df['PostedBy'] == postedby)
+                & (df['ReraApproved'] == reraapproved)
+                & (df['AreaName'] == areaname)
+                ]
 
+st.write("Filtered Data:")
+st.dataframe(filtered_df)
